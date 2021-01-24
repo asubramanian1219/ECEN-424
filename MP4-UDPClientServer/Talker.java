b01b973493d54989db2c2b371e33d6080d67a3f0
asubramanian1219 <45539219+asubramanian1219@users.noreply.github.com>
2021-01-23 18:18:25 -0600

    Add files via upload

--- MP4-UDPClientServer/Talker.java
+++ MP4-UDPClientServer/Talker.java
@@ -1,1 +1,58 @@
+import java.net.* ;
 
+/**
+ *  A simple datagram server
+ *  Shows how to send and receive UDP packets in Java
+ *
+ */
+public class Talker
+{
+   private final static int PACKETSIZE = 100 ;
+
+   public static void main( String args[] )
+   {
+      // Check the arguments
+      if( args.length != 2 )
+      {
+         System.out.println( "usage: DatagramServer port message" ) ;
+         return ;
+      }
+
+      try
+      {
+         // Convert the argument to ensure that is it valid
+         int port = Integer.parseInt( args[0] ) ;
+         String bigMessage   = args[2];
+
+         if(bigMessage.length()>50){
+             throw new Exception("Message too long.");
+         }
+          String[] messages= bigMessage.split("(?<=\\G..........)");
+
+         // Construct the socket
+         DatagramSocket socket = new DatagramSocket( port ) ;
+
+         System.out.println( "The server is ready..." ) ;
+
+
+         for( ;; )
+         {
+            // Create a packet
+            DatagramPacket packet = new DatagramPacket( new byte[PACKETSIZE], PACKETSIZE ) ;
+
+            // Receive a packet (blocking)
+            socket.receive( packet ) ;
+
+            // Print the packet
+            System.out.println( packet.getAddress() + " " + packet.getPort() + ": " + new String(packet.getData()) ) ;
+
+            // Return the packet to the sender
+            socket.send( packet ) ;
+        }  
+     }
+     catch( Exception e )
+     {
+        System.out.println( e ) ;
+     }
+  }
+}
